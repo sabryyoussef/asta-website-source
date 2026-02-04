ac4a1d489289da11d167a9a98e2d2e19c8ebbfba
AbdelRahman <AbdelRahman>
2026-02-04 16:26:58 +0300

    Update CategoryHeroSection to use categoryPNG instead of categorywebp for image source

--- src/components/Courses/CategoryHeroSection.jsx
+++ src/components/Courses/CategoryHeroSection.jsx
@@ -88,7 +88,7 @@ const CategoryHeroSection = ({ categoryName, categoryDescription, categoryImage,
 
           {/* Image */}
           <div className="relative">
-            <img src={categorywebp || '/images/IT_lock.webp'} alt={categoryName} className="rounded-2xl shadow-lg w-full h-auto object-cover col-span-2 lg:col-span-1" />
+            <img src={categoryPNG || '/images/IT_lock.webp'} alt={categoryName} className="rounded-2xl shadow-lg w-full h-auto object-cover col-span-2 lg:col-span-1" />
 
             {/* Floating Cards */}
             <div className={`absolute -top-6 ${isRTL ? '-left-6' : '-right-6'} w-64 bg-gradient-to-r from-[#23A0D0] to-[#30AFC1] rounded-2xl p-6 shadow-2xl transform ${isRTL ? 'rotate-3' : '-rotate-3'}`}>
